dd184706b98ddfe3cfb022941bf9a48dc3b8730e
kzak <sakadonohito@gmail.com>
2021-11-29 17:11:49 +0900

    posts/index.mdx内のファイル名修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://portfolio.k-zak.com",
     title: "kzak's practice gallery",
   },
   plugins: [
